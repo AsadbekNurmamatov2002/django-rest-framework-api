e1e4182f97c6b09a85c82603fcdb98691ccef4e0
AsadbekNurmamatov2002 <nurmamatovasadbek85@gmail.com>
2024-03-26 10:30:15 +0500

    m1

--- app/product/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms 
-from .models import Orders
-
-class OrderCreateForm(forms.ModelForm):
-    class Meta:
-        model=Orders
-        fields = "__all__"
-        # exclude=('product','xomashyo')
-
--- app/product/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name="product"
-urlpatterns=[
-    path("",Home,name="home"),
-    path("product-detail/<str:id>/<slug:slug>/",product_detail, name="product_detail"),
-
-]
